a48cf1dc6c25f01985988a3c69a9ca4fe6744609
MonowarShoz <shozolhorn@gmail.com>
2026-01-22 18:16:36 +0600

    some api impl

--- lib/features/Accounts/data/repository/accounts_repository_impl.dart
+++ lib/features/Accounts/data/repository/accounts_repository_impl.dart
@@ -15,19 +15,15 @@ import '../../../../core/networking/api_error_handler.dart';
 import '../../../Home/data/datasource/local/home_local_data_source.dart';
 import '../datasource/remote/accounts_remote_dt_source.dart';
 
-class AccountRepositoryImpl extends AccountsRepository{
+class AccountRepositoryImpl extends AccountsRepository {
   final HomeLocalDataSource localDataSource;
   final AccountsRemoteDataSource accountsRemoteDataSource;
+
   AccountRepositoryImpl({required this.localDataSource, required this.accountsRemoteDataSource});
-  @override
-  Future<ApiResult<List<GetBalanceWithdrawModel>>> getBalWithdrawData({required int compID}) {
-    // TODO: implement getBalWithdrawData
-    throw UnimplementedError();
-  }
 
   @override
-  Future<ApiResult<String?>> saveBalanceSegmentInfo({required BalanceSegmentSaveModel balanceSegmentBody}) async{
-    try{
+  Future<ApiResult<List<GetBalanceWithdrawModel>>> getBalWithdrawData({required int compID}) async {
+    try {
       final token = await localDataSource.getToken();
       final userData = await localDataSource.getUserInformation();
       if ((token?.isEmpty ?? true) && (userData?.isEmpty ?? true)) {
@@ -37,33 +33,109 @@ class AccountRepositoryImpl extends AccountsRepository{
         );
       }
       final Map userJson = jsonDecode(userData!);
-      final result = await accountsRemoteDataSource.saveBalanceSegmentInfo(token: 'Bearer $token', balanceSegmentBody: BalanceSegmentSaveModel(
-        id: balanceSegmentBody.id,
-        compId: userJson['cid'],
-        vendor: balanceSegmentBody.vendor,
-        amount: balanceSegmentBody.amount,
-        createDate: balanceSegmentBody.createDate,
-        descri: balanceSegmentBody.descri
-
-      ));
-      return result.when(success: (data) {
-        if(data != null){
-          return ApiResult.success(data);
-        }else{
-          return ApiResult.failure(ApiErrorHandler.handle('No content'));
-        }
-      }, failure: (errorHandler) {
-       return ApiResult.failure(errorHandler);
-      },);
-    }catch(e){
+      final result = await accountsRemoteDataSource.getBalWithdrawData(token: 'Bearer $token', compID: userJson['cid']);
+      return result.when(
+        success: (data) {
+          if (data != null) {
+            return ApiResult.success(data);
+          } else {
+            return ApiResult.failure(ApiErrorHandler.handle(ApiErrors.noContent));
+          }
+        },
+        failure: (errorHandler) {
+          return ApiResult.failure(ApiErrorHandler.handle(errorHandler));
+        },
+      );
+    } catch (e) {
       return ApiResult.failure(ApiErrorHandler.handle(e));
     }
   }
 
   @override
-  Future<ApiResult<String?>> saveBalanceWithdraw({required SaveWithDrawModel saveWithdrawData}) {
-    // TODO: implement saveBalanceWithdraw
-    throw UnimplementedError();
+  Future<ApiResult<String?>> saveBalanceSegmentInfo({required BalanceSegmentSaveModel balanceSegmentBody}) async {
+    try {
+      final token = await localDataSource.getToken();
+      final userData = await localDataSource.getUserInformation();
+      if ((token?.isEmpty ?? true) && (userData?.isEmpty ?? true)) {
+        return ApiResult.failure(
+          ApiErrorHandler.handle(ApiErrors.unauthorizedError),
+          //ApiErrorHandler.handle(ApiErrorModel(code: 401,message: 'UnAuthorized')),
+        );
+      }
+      final Map userJson = jsonDecode(userData!);
+      final result = await accountsRemoteDataSource.saveBalanceSegmentInfo(
+        token: 'Bearer $token',
+        balanceSegmentBody: BalanceSegmentSaveModel(
+          id: balanceSegmentBody.id,
+          compId: userJson['cid'],
+          vendor: balanceSegmentBody.vendor,
+          amount: balanceSegmentBody.amount,
+          createDate: balanceSegmentBody.createDate,
+          descri: balanceSegmentBody.descri,
+        ),
+      );
+      return result.when(
+        success: (data) {
+          if (data != null) {
+            return ApiResult.success(data);
+          } else {
+            return ApiResult.failure(ApiErrorHandler.handle('No content'));
+          }
+        },
+        failure: (errorHandler) {
+          return ApiResult.failure(errorHandler);
+        },
+      );
+    } catch (e) {
+      return ApiResult.failure(ApiErrorHandler.handle(e));
+    }
+  }
+
+  @override
+  Future<ApiResult<String?>> saveBalanceWithdraw({required SaveWithDrawModel saveWithdrawData}) async {
+    try {
+      final token = await localDataSource.getToken();
+      final userData = await localDataSource.getUserInformation();
+      if ((token?.isEmpty ?? true) && (userData?.isEmpty ?? true)) {
+        return ApiResult.failure(
+          ApiErrorHandler.handle(ApiErrors.unauthorizedError),
+          //ApiErrorHandler.handle(ApiErrorModel(code: 401,message: 'UnAuthorized')),
+        );
+      }
+      final Map userJson = jsonDecode(userData!);
+      final result = await accountsRemoteDataSource.saveBalanceWithdraw(
+        token: 'Bearer $token',
+        saveWithdrawData: SaveWithDrawModel(
+          id: saveWithdrawData.id,
+          compId: userJson['cid'],
+          fProject: saveWithdrawData.fProject,
+          remarks: saveWithdrawData.remarks,
+          wBy: userJson['username'],
+          wDate: saveWithdrawData.wDate,
+          //""
+          wYear: saveWithdrawData.wYear,
+          //2026
+          wMonth: saveWithdrawData.wMonth,
+          //January
+          amount: saveWithdrawData.amount,
+          memNo: saveWithdrawData.memNo,
+        ),
+      );
+      return result.when(
+        success: (data) {
+          if (data != null) {
+            return ApiResult.success(data);
+          } else {
+            return ApiResult.failure(ApiErrorHandler.handle('No content'));
+          }
+        },
+        failure: (errorHandler) {
+          return ApiResult.failure(errorHandler);
+        },
+      );
+    } catch (e) {
+      return ApiResult.failure(ApiErrorHandler.handle(e));
+    }
   }
 
   @override
@@ -89,5 +161,4 @@ class AccountRepositoryImpl extends AccountsRepository{
     // TODO: implement saveKistiReceive
     throw UnimplementedError();
   }
-
 }
